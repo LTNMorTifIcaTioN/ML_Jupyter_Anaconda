514eeaa8ecdf9cf4c4f25c76cdabababbacb8314
LTNMorTifIcaTioN <pereira.nayan@gmail.com>
2024-04-15 01:16:23 -0300

    69_Exploracao.ipynb

---
+++ Secao_5_AS_Regressao/67_Regressao.py
@@ -0,0 +1,18 @@
+# 67. Regressão
+
+"""
+Utiliza os dados de entrada para treinar um algoritmo de Machine Learning e, assim, prever uma resposta numérica.
+    DADOS DE ENTRADA → ALGORITMO DE APRENDIZAGEM → PREVISÃO
+
+Algoritmos de Regressão
+    > Regressão Linear Simples
+    > Regressão Linear Multipla
+    > Árvore de Decisão
+    > Random Forest
+    > Regressão SVM
+    > XGBoost
+    > LightGBM
+    > CatBoost
+    > Redes Neurais Artificiais
+"""
+#%%
